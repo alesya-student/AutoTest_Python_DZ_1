6bace1cdfd4c53fbba6883f553cdf03a01e7fa15
alesya-student <git config --global user.email>
2024-09-04 11:09:49 +0300

    add first homework

--- lesson1/test.py
+++ lesson1/test.py
@@ -1,1 +1,10 @@
-print ("Привет!")
+# print ("Привет!")
+
+
+# imports are studied on Hexlet
+import random
+
+# BEGIN (write your solution here)
+random_float = random.random() * 10
+print(round(random_float))
+# END
--- lesson2/lesson_2_task_4.py
+++ lesson2/lesson_2_task_4.py
@@ -1,15 +1,16 @@
-n = int()
-def fizz_buzz():
-    if (n % 3 == 0) and (n % 5 == 0):
-        print('FizzBuzz')
+n = int(input())
+def fizz_buzz(n):
+    for n in range(1, n+1): # в этом случае отработают все числа из промежутка от 1 до заданного
+        if (n % 3 == 0) and (n % 5 == 0):
+            print('FizzBuzz')
     
-    elif (n % 3 == 0):
-        print('Buzz')
+        elif (n % 3 == 0):
+            print('Buzz')
 
-    elif (n % 5 == 0):
-        print('Fizz')
+        elif (n % 5 == 0):
+            print('Fizz')
 
-    else:
-        print(n)
+        else:
+            print(n)
 
-fizz_buzz()
+fizz_buzz(n)
